9a6191cb9db58addbb1c42ae687154c8bf773cf4
Seun Olafisoye <86838623+olafisoyevince@users.noreply.github.com>
2026-04-03 22:03:03 +0100

    Add bobbing effect to pickups and refine weapon raycasting logic
    - Implemented a bobbing effect for pickups using sine wave calculations for vertical movement.
    - Added serialized fields for bob height and speed to allow customization in the inspector.
    - Refined the raycasting logic in the Weapon class to ignore trigger colliders, improving shooting accuracy.

--- Assets/Scripts/Pickups/Pickup.cs
+++ Assets/Scripts/Pickups/Pickup.cs
@@ -2,7 +2,22 @@ using UnityEngine;
 
 public abstract class Pickup : MonoBehaviour
 {
+    [SerializeField] float bobHeight = 0.15f;
+    [SerializeField] float bobSpeed = 2f;
     const string PLAYER_STRING = "Player";
+    Vector3 startPosition;
+
+    void Awake()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float yOffset = Mathf.Sin(Time.time * bobSpeed) * bobHeight;
+        transform.position = startPosition + Vector3.up * yOffset;
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(PLAYER_STRING))
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,7 +4,6 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] ParticleSystem muzzleFlashSpark;
     [SerializeField] ParticleSystem muzzleFlashGlow;
-
     public void PlayerShoot(WeaponSO weaponSO)
     {
 
@@ -14,9 +13,8 @@ public class Weapon : MonoBehaviour
 
         RaycastHit hit;
 
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity, Physics.DefaultRaycastLayers, QueryTriggerInteraction.Ignore))
         {
-
             Instantiate(weaponSO.HitVFX, hit.point, Quaternion.identity);
 
             EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
